bafbed5052420a79a82f3f4128f660bae1479e99
HoangTan1205 <tanhvph53409@gmail.com>
2024-10-03 22:52:58 +0700

    Add_Enemy

---
+++ Assets/Script/Linh/Linh_Xa_Thu.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Linh_Xa_Thu : Linh_Manager
+{
+    private Rigidbody2D rig;
+    public float move = 250f;
+
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        Move(Vector2.right, move, rig);
+    }
+}
---
+++ Assets/Script/Manager/Linh_Manager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Linh_Manager : MonoBehaviour
+{
+    
+    // Hàm di chuyển
+    public virtual void Move(Vector2 direction, float moveSpeed, Rigidbody2D rig)
+    {
+        Vector3 move = new Vector3(direction.x, direction.y,0) * moveSpeed * Time.deltaTime;
+        rig.velocity = move;
+
+    }
+}
